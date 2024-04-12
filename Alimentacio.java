d7e0053ce94adf6358e7344a1afede6197fcde62
luis0 <luis010993@gmail.com>
2024-04-12 12:35:07 +0200

    Creacio clase Alimentacio

---
+++ Alimentacio.java
@@ -0,0 +1,25 @@
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class Alimentacio extends Producte {
+    private LocalDate dataCaducitat;
+
+    public Alimentacio(float preu, String nom, int codiBarres, LocalDate data) {
+        super(preu, nom, codiBarres);
+        this.dataCaducitat = data;
+    }
+
+    @Override
+    public float getPreu() {
+
+        float preu = super.getPreu();
+        LocalDate dataActual = LocalDate.now();
+
+        long diferencia = dataActual.until(dataCaducitat).getDays();
+        return preu - (float) (preu * (1 / (diferencia + 1)) + (preu * (0.1)));
+    }
+
+    public LocalDate getDataCaducitat() {
+        return dataCaducitat;
+    }
+}
