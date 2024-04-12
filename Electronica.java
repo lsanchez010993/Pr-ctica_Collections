8c76c0883f2dd8f66b6e6997cded943bdd2ff1e2
luis0 <luis010993@gmail.com>
2024-04-12 12:51:52 +0200

    Creacio clase Electronica

---
+++ Electronica.java
@@ -0,0 +1,15 @@
+public class Electronica extends Producte{
+    private int dies_garantia;
+
+    public Electronica(float preu, String nom, int codiBarres, int dies_garantia) {
+        super(preu, nom, codiBarres);
+        this.dies_garantia = dies_garantia;
+    }
+
+    public float getPreu() {
+        float preu = super.getPreu();
+        return (float) (preu + preu*(dies_garantia/365)*0.1);
+    }
+
+
+}
